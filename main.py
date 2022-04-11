af637b3205f16fce9e2bf5c9deae32778f012f0c
Gabriel Barrientos <gabrielbarrientos3@gmail.com>
2022-04-11 06:50:37 -0300

    Add arg parsing, logging and send mail

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+# Parseo los argumentos
+import argparse
+from datetime import datetime
+import logging
+import os
+import traceback
+
+from utils.sendMail import sendMail
+
+parser = argparse.ArgumentParser(description="Analitica collect")
+parser.add_argument("-s", action="store", dest="proceso", help="Store a simple value")
+results = parser.parse_args()
+
+
+# Logging Config
+d = datetime.now()
+logpath = os.getcwd() + "/logs/" + d.strftime('%Y%m%d')
+os.makedirs(logpath, exist_ok=True)
+logfilename = logpath + "/" + d.strftime('%Y%m%d%H%M%S') + "_" + results.proceso + ".log"
+logname = d.strftime('%Y%m%d%H%M%S') + "_" + results.proceso + ".log"
+print('Logging in: ' + logfilename)
+logging.basicConfig(level=logging.INFO, filename=logfilename, format='%(levelname)s:%(asctime)s:%(funcName)s:%(lineno)d:%(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+
 from connectors.ConnectorFactory import ConnectorFactory
 
 aConnectorFactory = ConnectorFactory()
-aConnector = aConnectorFactory.createConnector('facebook')
-aConnector.execute()
+aConnector = aConnectorFactory.createConnector(results.proceso)
+
+try:
+	aConnector.execute()
+
+except Exception as e:
+	logging.error(e, exc_info=True)
+	traceback.print_exc()
+	with open(logfilename, 'r') as file:
+		log = file.read()
+		sendMail(logname,log)
+
+logging.info('FIN')
+exit()
