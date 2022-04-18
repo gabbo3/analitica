6064e5a45fff012834dbae6a1876ad87695b0c68
Gabriel Barrientos <gabrielbarrientos3@gmail.com>
2022-04-18 17:35:16 -0300

    Agrego Instagram y Google Ad Server

--- connectors/ConnectorFactory.py
+++ connectors/ConnectorFactory.py
@@ -1,13 +1,19 @@
 from connectors.Connector import Connector
 from facebookSource.ConnectorFacebookDiario import ConnectorFacebookDiario
 from facebookSource.ConnectorFacebookInsights import ConnectorFacebookInsights
+from googleAdServer.ConnectorGoogleAd import ConnectorGoogleAd
+from instagram.ConnectorInstagram import ConnectorInstagram
 
 
 class ConnectorFactory:
     def createConnector(self, name):
         if name == 'facebook':
             return ConnectorFacebookDiario()
-        if name == 'facebookinsights':
+        elif name == 'facebookinsights':
             return ConnectorFacebookInsights()
+        elif name == 'instagram':
+            return ConnectorInstagram()
+        elif name == 'googleadserver':
+            return ConnectorGoogleAd()
         else:
             return Connector()
