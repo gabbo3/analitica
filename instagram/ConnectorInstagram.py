ec5a000fbc5313a2a7e650a6b50305d62807aa10
BUEANA <gabrielbarrientos3@gmail.com>
2022-04-23 14:21:18 -0300

    Agrego logging

--- instagram/ConnectorInstagram.py
+++ instagram/ConnectorInstagram.py
@@ -1,3 +1,4 @@
+import logging
 import pandas as pd
 from connectors.Connector import Connector
 from databases.MongoDB.MongoDB import MongoDB
@@ -15,6 +16,9 @@ class ConnectorInstagram(Connector):
 		## TODO: Add Business discovery para ver numeros de otras cuentas
 		# Para cada cuenta
 		for a in self.accounts:
+			logging.info('Procesando: ' + a.instagram_name)
+
+			logging.info('Recuperando posteos de : ' + a.instagram_name)
 			posts = a.getPosts(10)
 			posts_df = []
 			for p in posts:
@@ -22,6 +26,7 @@ class ConnectorInstagram(Connector):
 				posts_df.append(p.asSQLDict())
 			self.sql.upsert(pd.DataFrame(posts_df),'PY_IG_MEDIA')
 			
+			logging.info('Recuperando stories de : ' + a.instagram_name)
 			stories = a.getStories(10)
 			stories_df = []
 			for s in stories:
@@ -29,11 +34,13 @@ class ConnectorInstagram(Connector):
 				stories_df.append(s.asSQLDict())
 			self.sql.upsert(pd.DataFrame(stories_df),'PY_IG_STORIES')
 
+			logging.info('Recuperando Insights diarias de : ' + a.instagram_name)
 			insights = a.getDailyInsights(10)
 			for i in insights:
 				self.mongo.upsertDF(i.asRawDF(),'TESTE','InstagramInsights')
 				self.sql.upsert(i.asSQLDF(),'PY_IG_AccountInsights')
 
+			logging.info('Recuperando Insights complejas de : ' + a.instagram_name)
 			insights = a.getLifetimeInsights(10)
 			for i in insights:
 				self.mongo.upsertDF(i.asRawDF(),'TESTE','InstagramInsights')
