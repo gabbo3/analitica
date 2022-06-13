13c6ebc74ce547395d4e91773e44c6d4b39be31b
BUEANA <gabrielbarrientos3@gmail.com>
2022-06-13 18:26:40 -0300

    Mappeos de twitter

--- twitter/Mention.py
+++ twitter/Mention.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from mappings.twitter.mentions import MentionMap
 
 class Mention:
 	def __init__(self, data) -> None:
 		self.data = data
 
 	def asSQLDF(self):
-		return pd.json_normalize(self.data)
+		return MentionMap.sql(pd.json_normalize(self.data))
--- twitter/Tweet.py
+++ twitter/Tweet.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from mappings.twitter.tweets import TweetMap
 
 class Tweet:
 	def __init__(self, data : dict) -> None:
 		self.data = data
 
 	def asSQLDF(self):
-		return pd.json_normalize(self.data)
+		return TweetMap.sql(pd.json_normalize(self.data))
